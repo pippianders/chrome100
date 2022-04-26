3c7880d9b6ee98b1d23213ba974002ad6c7fae69
David Reed <x@sys32.dev>
2022-04-26 17:23:42 -0400

    add prettier, update consts, sync new data

--- cli/sync.js
+++ cli/sync.js
@@ -1,8 +1,15 @@
 import Sync from '../Sync.js';
+import DataStore from '../DataStore.js';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-export default async function sync(){
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default async function sync() {
+	const data = new DataStore(join(__dirname, '..', 'data.json'));
 	const sync = new Sync(data);
-	
+
 	console.log('Syncing...');
 
 	await sync.run();
